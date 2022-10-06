b6ec647befad71ec03156f15f8e538a5c26b2826
Francois LP <francois@remazing.eu>
2022-10-06 14:26:10 +0200

    add console log to debug

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send({ healthy: true }))
+app.get('/', (req, res) => {
+  console.log('request received on /')
+  res.send({ healthy: true })
+})
 
 app.set('trust proxy', true)
 
 app.get('/ip', (request, response) => {
+  const header = request.header('X-Forwarded-For') || null
+  console.log(`request received on /ip (${request.ip} ${header})`)
   return response.send({
     'request IP': request.ip,
     'request.socket.remoteAddress': request.socket.remoteAddress,
-    'X-Forwarded-For header': request.header('X-Forwarded-For') || null
+    'X-Forwarded-For header': header
   })
 })
 
